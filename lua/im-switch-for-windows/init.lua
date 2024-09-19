b37ae2493695242aaf6253c5e4a8200373282d3f
xiaojianzheng <1272209235@qq.com>
2024-09-19 17:58:25 +0800

    feat: support show colro in normal mode

--- lua/im-switch-for-windows/init.lua
+++ lua/im-switch-for-windows/init.lua
@@ -39,7 +39,7 @@ function M.switch_to_en()
   return change_im_select("en")
 end
 
-local function update_cursor_color()
+local function get_current_mode()
   local handle = io.popen("im-switch.exe", "r")
   local result = nil
 
@@ -48,25 +48,43 @@ local function update_cursor_color()
     handle:close()
   end
 
+  return result
+end
+
+local function update_cursor_color()
+  local result = get_current_mode()
   if M.input_state == result then
     return
   end
 
   local guicursor = "n-v-c:block-Cursor,"
 
-  if result == "caps" then
-    guicursor = guicursor .. "i-ci-ve:ver25-CursorCaps/lCursorCaps"
-    local color = M.opts.color.caps
-    vim.api.nvim_set_hl(0, "CursorCaps", { fg = color, bg = color })
-  elseif result == "zh" then
-    local color = M.opts.color.zh
-    guicursor = guicursor .. "i-ci-ve:ver25-CursorZh/lCursorZh"
-    vim.api.nvim_set_hl(0, "CursorZh", { fg = color, bg = color })
-  else
-    guicursor = M.opts.default_guicursor
+  local mode = vim.fn.mode()
+
+  if mode == "i" then
+    if result == "caps" then
+      guicursor = guicursor .. "i-ci-ve:ver25-CursorCaps/lCursorCaps"
+      local color = M.opts.color.caps
+      vim.api.nvim_set_hl(0, "CursorCaps", { fg = color, bg = color })
+    elseif result == "zh" then
+      local color = M.opts.color.zh
+      guicursor = guicursor .. "i-ci-ve:ver25-CursorZh/lCursorZh"
+      vim.api.nvim_set_hl(0, "CursorZh", { fg = color, bg = color })
+    else
+      guicursor = M.opts.default_guicursor
+    end
+    vim.opt.guicursor = guicursor
+  elseif mode == "n" then
+    local color = "NONE"
+    if result == "caps" then
+      color = M.opts.color.caps
+    elseif result == "zh" then
+      color = M.opts.color.zh
+    end
+    vim.api.nvim_set_hl(0, "Cursor", { fg = color, bg = color })
+    vim.opt.guicursor = M.opts.default_guicursor
   end
 
-  vim.opt.guicursor = guicursor
   M.input_state = result
 end
 
@@ -89,7 +107,7 @@ end
 function M.setup(opts)
   M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
 
-  vim.api.nvim_create_autocmd("InsertEnter", {
+  vim.api.nvim_create_autocmd("BufEnter", {
     pattern = "*",
     callback = M.start_hilight,
   })
@@ -97,7 +115,9 @@ function M.setup(opts)
   vim.api.nvim_create_autocmd("InsertLeave", {
     pattern = "*",
     callback = function()
-      M.stop_hilight()
+      if M.input_state ~= "en" then
+        M.switch_to_en()
+      end
     end,
   })
 end
